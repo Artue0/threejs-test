34d4ab3fa2eb828fdc2227382c31c6a3685cfe9e
Artue0 <128091223+Artue0@users.noreply.github.com>
2024-01-30 21:36:33 +0100

    1

--- main.js
+++ main.js
@@ -12,11 +12,25 @@ const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
 const icosahedronGeometry = new THREE.IcosahedronGeometry(1,0)
 const material = new THREE.MeshBasicMaterial( { color: 0x19C515, wireframe: true } );
 const cube = new THREE.Mesh( cubeGeometry, material );
+const cameraShape = new THREE.Mesh( icosahedronGeometry, material );
 const icosahedron = new THREE.Mesh( icosahedronGeometry, material );
-scene.add(icosahedron);
-icosahedron.position.z = -0.9
+scene.add(cameraShape);
+
+cameraShape.position.z = 4.2
+cube.position.x = 4.2
+icosahedron.position.x = -6
+icosahedron.position.y = 3
+camera.position.z = 5;
 
 const controls = new OrbitControls(camera, renderer.domElement)
+
+function moveCamera() {
+	const top = document.body.getBoundingClientRect().top;
+	cameraShape.rotation.x = top * 0.001;
+	cameraShape.rotation.y = top * 0.001;
+}
+
+document.body.onscroll = moveCamera
 
 function animate() {
 	requestAnimationFrame( animate );
@@ -24,8 +38,6 @@ function animate() {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-	icosahedron.rotation.x += 0.01;
-	icosahedron.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
