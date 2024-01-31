69f7c8a53d7537c969dc4d6e83cd8ebbca1034a2
Artue0 <128091223+Artue0@users.noreply.github.com>
2024-01-31 22:00:49 +0100

    6

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
-const icosahedronGeometry = new THREE.IcosahedronGeometry(1,100)
+const icosahedronGeometry = new THREE.IcosahedronGeometry(1,0)
 const material = new THREE.MeshBasicMaterial( { color: 0x19C515, wireframe: true } );
 const cube = new THREE.Mesh( cubeGeometry, material );
 const cameraShape = new THREE.Mesh( icosahedronGeometry, material );
