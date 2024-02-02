32275cc71019d6bc51b57579c8318fc7d9f82328
Artue0 <128091223+Artue0@users.noreply.github.com>
2024-02-02 13:18:32 +0100

    3

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ cameraShape.position.z = 4.2
 cube.position.x = 4.2
 icosahedron.position.x = -6
 icosahedron.position.y = 3
-camera.position.z = 5;
+camera.position.z = 5
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
@@ -71,3 +71,28 @@ function animate() {
 }
 
 animate();
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var customCursor = document.getElementById('custom-cursor');
+
+    document.addEventListener('mousemove', function (e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        customCursor.style.left = x + 'px';
+        customCursor.style.top = y + 'px';
+    });
+
+    document.addEventListener('mouseleave', function () {
+        customCursor.style.display = 'none';
+    });
+
+    document.addEventListener('mouseenter', function () {
+        customCursor.style.display = 'block';
+    });
+});
