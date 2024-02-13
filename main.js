2f22c95e80a2965cd914c055437fb833683fa0c2
Artue0 <128091223+Artue0@users.noreply.github.com>
2024-02-13 17:26:28 +0100

    as

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { degToRad } from 'three/src/math/MathUtils';
 
-const monkeyUrl = new URL('assets/monkey.glb', import.meta.url);
+const shipUrl = new URL('assets/spaceship.glb', import.meta.url);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -19,17 +20,23 @@ const cameraShape = new THREE.Mesh( icosahedronGeometry, material );
 const icosahedron = new THREE.Mesh( icosahedronGeometry, material );
 scene.add(cameraShape);
 
-cameraShape.position.z = 4.2
+cameraShape.position.z = 5
 cube.position.x = 4.2
 icosahedron.position.x = -6
 icosahedron.position.y = 3
 camera.position.z = 5
 
 const assetLoader = new GLTFLoader();
+let ship2;
 
-assetLoader.load(monkeyUrl.href, function(gltf) {
-    const monkey = gltf.scene;
-    scene.add(monkey);
+assetLoader.load(shipUrl.href, function(gltf) {
+    const ship = gltf.scene;
+    const shipMesh = ship.children[0];
+    shipMesh.material = material;
+    scene.add(ship);
+    ship.position.z = 5;
+    ship.position.y = -15;
+    ship2 = ship;
 }, undefined, function(error) {
     console.error(error)
 });
@@ -50,11 +57,49 @@ window.addEventListener('scroll', function() {
     }
 });
 
+var cameraStage = 0;
+var change = true;
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    console.log(camera.rotation.x)
+    switch(cameraStage) {
+        case 0:
+            camera.rotation.x = Math.max(t * 0.001, -Math.PI/2);
+
+            if (camera.rotation.x <= -Math.PI/2 && change) {
+                cameraStage = 1;
+                console.log("a")
+            }
+            break;
+        case 1:
+            camera.position.y = Math.max(t * 0.005 + 5, -15.6);
+            change = false;
+            if (camera.position.y > -10) {
+                cameraStage = 1;
+            } else if (camera.position.y >= -3) {
+                cameraStage = 0;
+                console.log("b")
+            }
+            break;
+        case 2:
+            camera.position.z = Math.max(t * 0.005 + 5, -15.6);
+
+            if (camera.position.z > -10) {
+                cameraStage = 1;
+            }
+            break;
+    }
+}
+  
+document.body.onscroll = moveCamera;
+moveCamera();
+
 let lastScrollTop = 0;
 
 window.addEventListener('scroll', function() {
     let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    
+    /*
     if (currentScroll > lastScrollTop) {
         cameraShape.rotation.x += 0.008;
 		cameraShape.rotation.y += 0.008;
@@ -62,7 +107,7 @@ window.addEventListener('scroll', function() {
         cameraShape.rotation.x -= 0.008;
 		cameraShape.rotation.y -= 0.008;
     }
-    
+    */
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 }, false);
 
@@ -79,6 +124,10 @@ function animate() {
 		cameraShape.rotation.y += 0.008;
 	}
 
+    if(ship2){
+        ship2.rotation.y += 0.008;
+    }
+
 	renderer.render( scene, camera );
 }
 
@@ -93,21 +142,12 @@ window.addEventListener('resize', function() {
 document.addEventListener('DOMContentLoaded', function () {
     var customCursor = document.getElementById('custom-cursor');
 
-    function updateCursorPosition(e) {
-        var x, y;
-        if (e.type === 'touchmove') {
-            x = e.touches[0].clientX + window.scrollX;
-            y = e.touches[0].clientY + window.scrollY;
-        } else {
-            x = e.clientX + window.scrollX;
-            y = e.clientY + window.scrollY;
-        }
+    document.addEventListener('mousemove', function (e) {
+        var x = e.clientX;
+        var y = e.clientY;
         customCursor.style.left = x + 'px';
         customCursor.style.top = y + 'px';
-    }
-
-    document.addEventListener('mousemove', updateCursorPosition);
-    document.addEventListener('touchmove', updateCursorPosition);
+    });
 
     document.addEventListener('mouseleave', function () {
         customCursor.style.display = 'none';
