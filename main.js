e505fc8c431121b90fd834381e6699d35f314b46
Artue0 <128091223+Artue0@users.noreply.github.com>
2024-03-27 23:11:10 +0100

    s

--- main.js
+++ main.js
@@ -1,134 +1,73 @@
 import * as THREE from 'three';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { degToRad } from 'three/src/math/MathUtils';
-
-const shipUrl = new URL('assets/spaceship.glb', import.meta.url);
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
-const icosahedronGeometry = new THREE.IcosahedronGeometry(1)
-const material = new THREE.MeshBasicMaterial( { color: 0x19C515, wireframe: true } );
-const cube = new THREE.Mesh( cubeGeometry, material );
-const cameraShape = new THREE.Mesh( icosahedronGeometry, material );
-const icosahedron = new THREE.Mesh( icosahedronGeometry, material );
-scene.add(cameraShape);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-cameraShape.position.z = 5
-cube.position.x = 4.2
-icosahedron.position.x = -6
-icosahedron.position.y = 3
-camera.position.z = 5;
+const whiteMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
+const blackMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
 
-const assetLoader = new GLTFLoader();
-let ship2;
+const boxes = [];
+const frameLinesArray = [];
 
-assetLoader.load(shipUrl.href, function(gltf) {
-    const ship = gltf.scene;
-    const shipMesh = ship.children[0];
-    shipMesh.material = material;
-    scene.add(ship);
-    ship.position.z = 5;
-    ship.position.y = -15;
-    ship2 = ship;
-}, undefined, function(error) {
-    console.error(error)
-});
+for (let i = 0; i < 350; i++) {
+    const [x, y, z, x2, y2, z2] = Array(6).fill().map(() => THREE.MathUtils.randFloatSpread(200));
+    const [rot_x, rot_y, rot_z] = Array(3).fill().map(() => Math.random() * 360);
+    const scale = Math.random() * (4 - 0.3) + 0.3;
+    const star_geometry = new THREE.SphereGeometry(0.2, 24, 24);
+    const star = new THREE.Mesh(star_geometry, whiteMaterial);
+    const box_geometry = new THREE.BoxGeometry(2.03, 2.03, 2.03);
+    const box = new THREE.Mesh(box_geometry, blackMaterial);
 
-const controls = new OrbitControls(camera, renderer.domElement)
+    const frameGeometry = new THREE.BoxGeometry(2, 2, 2);
+    const frameEdges = new THREE.EdgesGeometry(frameGeometry);
+    const frameLines = new THREE.LineSegments(frameEdges, whiteMaterial);
 
-function isPageScrolledToTop() {
-    return (document.documentElement.scrollTop || document.body.scrollTop) === 0;
+    frameLines.rotation.set(rot_x, rot_y, rot_z);
+    frameLines.scale.set(scale, scale, scale);
+    frameLines.position.set(x2, y2, z2);
+
+    frameLines.scale.multiplyScalar(1 + 0.02);
+
+    box.rotation.set(rot_x, rot_y, rot_z);
+    box.scale.set(scale, scale, scale);
+    box.position.set(x2, y2, z2);
+
+    star.position.set(x, y, z);
+
+    scene.add(star, box, frameLines);
+
+    const rotationSpeed = Math.random() * 0.005;
+
+    boxes.push({ boxMesh: box, rotationSpeed: rotationSpeed });
+    frameLinesArray.push({ linesMesh: frameLines, rotationSpeed: rotationSpeed })
 }
 
-let rotate = true;
-
-window.addEventListener('scroll', function() {
-    if (isPageScrolledToTop()) {
-        rotate = true;
-    } else {
-        rotate = false;
-    }
-});
-
-var cameraStage = 0;
-var change = true;
-
-function moveCamera() {
-    const t = document.body.getBoundingClientRect().top;
-    console.log(camera.rotation.x)
-    switch(cameraStage) {
-        case 0:
-            camera.rotation.x = Math.max(t * 0.001, -Math.PI/2);
-
-            if (camera.rotation.x <= -Math.PI/2 && change) {
-                cameraStage = 1;
-                console.log("a")
-            }
-            break;
-        case 1:
-            camera.position.y = Math.max(t * 0.005 + 5, -15.6);
-            change = false;
-            if (camera.position.y > -10) {
-                cameraStage = 1;
-            } else if (camera.position.y >= -3) {
-                cameraStage = 0;
-                console.log("b")
-            }
-            break;
-        case 2:
-            camera.position.z = Math.max(t * 0.005 + 5, -15.6);
-
-            if (camera.position.z > -10) {
-                cameraStage = 1;
-            }
-            break;
-    }
-}
-  
-document.body.onscroll = moveCamera;
-moveCamera();
-
-let lastScrollTop = 0;
-
-window.addEventListener('scroll', function() {
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    /*
-    if (currentScroll > lastScrollTop) {
-        cameraShape.rotation.x += 0.008;
-		cameraShape.rotation.y += 0.008;
-    } else {
-        cameraShape.rotation.x -= 0.008;
-		cameraShape.rotation.y -= 0.008;
-    }
-    */
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-}, false);
-
+camera.position.z = 10;
 
 function animate() {
-	requestAnimationFrame( animate );
-	//controls.update
+    requestAnimationFrame(animate);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+    boxes.forEach(boxInfo => {
+        const { boxMesh, rotationSpeed } = boxInfo;
+        boxMesh.rotation.x += rotationSpeed;
+        boxMesh.rotation.y += rotationSpeed;
+        boxMesh.rotation.z += rotationSpeed;
+    });
 
-	if(rotate){
-		cameraShape.rotation.x += 0.008;
-		cameraShape.rotation.y += 0.008;
-	}
+    frameLinesArray.forEach(linesInfo => {
+        const { linesMesh, rotationSpeed } = linesInfo;
+        linesMesh.rotation.x += rotationSpeed;
+        linesMesh.rotation.y += rotationSpeed;
+        linesMesh.rotation.z += rotationSpeed;
+    });
 
-    if(ship2){
-        ship2.rotation.y += 0.008;
-    }
-
-	renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
 
 animate();
